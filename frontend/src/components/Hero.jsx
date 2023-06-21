4816f2259553421efaef0ea76da93b44db74498a
AliAksoyy <alifeyza4017@gmail.com>
2023-06-22 01:00:06 +0300

    added

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -5,7 +5,10 @@ const Hero = () => {
   return (
     <div className="py-5">
       <Container className="d-flex justify-content-center">
-        <Card className="p-5 d-flex flex-column aling-items-center hero-card bg-light w-75"></Card>
+        <Card className="p-5 d-flex flex-column aling-items-center hero-card bg-light w-75">
+        1.44
+
+        </Card>
       </Container>
     </div>
   );
