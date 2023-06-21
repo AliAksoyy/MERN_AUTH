e58dbc44ef11058bc0dfba1ded53dce1e3d7143a
AliAksoyy <alifeyza4017@gmail.com>
2023-06-22 00:34:12 +0300

    added navbar

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Mern Auth</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/login">
+                <FaSignInAlt /> Sign In
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <FaSignOutAlt /> Sign up
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
